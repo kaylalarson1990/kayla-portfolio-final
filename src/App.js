9c7b18a04cda2f1b1a249e7677c596add8e523ef
Kayla <kayla.larson1990@gmail.com>
2019-08-29 20:15:01 -0600

    Start working on home page and get scroll functionality to work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
-import Header from './Header';
-import './App.css';
+import React from "react";
+import Header from "./Header";
+import ScrollUpButton from "react-scroll-up-button";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <div className="wrapper">
+      <div>
+        <ScrollUpButton
+          ContainerClassName="scroll-arrow"
+          EasingType="easeInOutBack"
+          ShowAtPosition={30}
+          AnimationDuration={500}
+        />
+      </div>
+      <section className="nav-section">
+        <div className="App">
+          <Header />
+        </div>
+      </section>
+      <section>
+        <h3 className="intro">
+          Hi! I'm <span>Kay</span>, an adventurous front end dev.
+        </h3>
+        <h3 className="intro-blurb">
+          {/* //{proficency examples} */}
+          As a developer, I enjoy using my thorough communication skills, my
+          strong passion for continous learning, and my unrelenting drive to
+          help others. I have a vast proficiency in problem solving and a
+          curious mind to help guide and motivate me.
+        </h3>
+      </section>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -4,32 +4,49 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <header>
-          <nav>
-            <ul className="nav">
-              <li>
-                <a href="#">
-                  <NavLink to="/home" exact activeClassName="active" className="home-page">
-                    Kayla Larson
-                  </NavLink>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <NavLink to="/work" exact activeClassName="active" className="head work-page">
-                    Work
-                  </NavLink>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <NavLink to="/about" exact activeClassName="active" className="head about-page">
-                    About
-                  </NavLink>
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </header>
+      <nav>
+        <ul className="nav">
+          <li>
+            <a href="#">
+              <NavLink
+                to="/home"
+                exact
+                activeClassName="active"
+                className="home-page"
+              >
+                Kayla Larson
+              </NavLink>
+            </a>
+          </li>
+          <div className="work-about-links">
+            <li>
+              <a href="#">
+                <NavLink
+                  to="/work"
+                  exact
+                  activeClassName="active"
+                  className="head work-page"
+                >
+                  WORK
+                </NavLink>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <NavLink
+                  to="/about"
+                  exact
+                  activeClassName="active"
+                  className="head about-page"
+                >
+                  ABOUT
+                </NavLink>
+              </a>
+            </li>
+          </div>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
